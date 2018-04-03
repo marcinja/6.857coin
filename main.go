69f8f6237bbdc4516c4b73329ed2524f2e93f002
Marcin Jachymiak <marcinja@mit.edu>
2018-04-02 22:33:47 -0400

    Correctly compute A and B

--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"bytes"
-	//	"crypto/aes"
+	"crypto/aes"
 	"crypto/sha256"
 	"encoding/binary"
 	"encoding/hex"
@@ -15,6 +15,7 @@ import (
 const TEAM_NAME = "marcinja"
 const NODE_URL = "http://6857coin.csail.mit.edu"
 const TIME_RANGE = 119
+const AES_BLOCK_SIZE = 16
 
 type Header struct {
 	ParentID   string    `json:"parentid`
@@ -95,8 +96,8 @@ func test1() {
 }
 
 // return SHA256 hash of header.
-func hashBlockHeader(header *Header) string {
-	// TODO: determine corrext size to allocate
+func hashBlockHeader(header *Header) []byte {
+	// TODO fix size to 32 and do copies instead?
 	b := make([]byte, 0)
 	h := sha256.New()
 	//	b, _ := json.Marshal(&header)
@@ -125,14 +126,86 @@ func hashBlockHeader(header *Header) string {
 
 	h.Write(b)
 	sum := h.Sum(nil)
-	hexStr := hex.EncodeToString(sum)
-	fmt.Println("Sum: ", hexStr)
 
-	return hexStr
+	//return hex.EncodeToString(sum)
+	fmt.Println(hex.EncodeToString(sum))
+	return sum
+}
+
+// getSeeds return seed and seed2 when given a header.
+func getSeeds(header *Header) ([]byte, []byte) {
+	b := make([]byte, 0)
+	h1 := sha256.New()
+
+	id, _ := hex.DecodeString(header.ParentID)
+	b = append(b, id[:]...)
+
+	root, _ := hex.DecodeString(header.Root)
+	b = append(b, root[:]...)
+
+	diffBuf := make([]byte, 8)
+	binary.BigEndian.PutUint64(diffBuf, header.Difficulty)
+	b = append(b, diffBuf[:]...)
+
+	tsBuf := make([]byte, 8)
+	binary.BigEndian.PutUint64(tsBuf, header.Timestamp)
+	b = append(b, tsBuf[:]...)
+
+	nonceBuf := make([]byte, 8)
+	binary.BigEndian.PutUint64(nonceBuf, header.Nonces[0])
+	b = append(b, nonceBuf[:]...)
+
+	b = append(b, byte(header.Version))
+
+	h1.Write(b)
+	seed := h1.Sum(nil)
+
+	// Now hash the first seed to get seed2.
+	h2 := sha256.New()
+	h2.Write(seed)
+	seed2 := h2.Sum(nil)
+
+	return seed, seed2
+}
+
+// TODO: check errors like a reasonable human being
+func computePoW(i, j uint64, seed, seed2 []byte) {
+	seedCipher, _ := aes.NewCipher(seed)
+	seed2Cipher, _ := aes.NewCipher(seed2)
+
+	// Plaintexts:
+	A_i_p := make([]byte, 16)
+	binary.BigEndian.PutUint64(A_i_p[8:], i)
+	A_j_p := make([]byte, 16)
+	binary.BigEndian.PutUint64(A_j_p[8:], j)
+	B_i_p := make([]byte, 16)
+	binary.BigEndian.PutUint64(B_i_p[8:], i)
+	B_j_p := make([]byte, 16)
+	binary.BigEndian.PutUint64(B_j_p[8:], j)
+
+	// Ciphertexts:
+	A_i := make([]byte, 16)
+	seedCipher.Encrypt(A_i, A_i_p)
+
+	A_j := make([]byte, 16)
+	seedCipher.Encrypt(A_j, A_j_p)
+
+	B_i := make([]byte, 16)
+	seedCipher.Encrypt(B_i, B_i_p)
+
+	B_j := make([]byte, 16)
+	seed2Cipher.Encrypt(B_j, B_j_p)
+
+	fmt.Println("CIPHERS")
+	fmt.Println(hex.EncodeToString(seed))
+	fmt.Println(hex.EncodeToString(A_j))
+	fmt.Println(hex.EncodeToString(B_i))
+	fmt.Println(hex.EncodeToString(B_j))
+
+	return
 }
 
 func makeNextBlock(header Header) Block {
-
 	newHeader := Header{
 		ParentID:   "", //TODO: SHA256 header,
 		Root:       "",
@@ -156,4 +229,17 @@ func main() {
 	//nextBlock()
 	//	addBlock(Block{})
 	test1()
+
+	i := uint64(999999999999)
+	j := uint64(999999999998)
+
+	seed := make([]byte, 32)
+	for i := 0; i < 32; i++ {
+		seed[i] = 0x30
+	}
+
+	fmt.Println("SEED:L", seed)
+	//	seed2 := make([]byte, 32)
+
+	computePoW(i, j, seed, seed)
 }
